6281d10460c03d738c5fbc4f1652115a68c35fa9
MrKrow <michael1kolanjian2@gmail.com>
2025-03-30 21:53:10 +0300

    Add the basic jsx code for login page

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,11 +1,33 @@
 import React from "react"
 import './LoginPage.css'
+import email_img from '../assets/email.svg'
+import password_img from '../assets/password.svg'
+
 
 export default function LoginPage() {
 
     return (
-        <div>
-
+        <div className="container">
+            <div className="header">
+                <div className="text">Login</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={email_img} alt="email svg"/>
+                    <input type="email" placeholder="Email"/>
+                </div>
+                <div className="input">
+                    <img src={password_img} alt="password svg"/>
+                    <input type="password" placeholder="Password"/>
+                </div>
+            </div>
+            <div className="submitContainer">
+                <div className="submit">
+                    Login
+                </div>
+            </div>
+            <div className="sign-up">Don't have an account? <span>Sign-up here!</span></div>
         </div>
     )
 }
---
+++ src/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import './SignUpPage.css'
+
+export default function SignUpPage() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
