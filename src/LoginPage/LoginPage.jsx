ac414879c1c5db879e1b4b2702846c30c59ae05b
Malek Baghdadi <malekbaghdade@gmail.com>
2025-03-31 00:53:24 +0300

    Added Logo

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -2,32 +2,40 @@ import React from "react"
 import './LoginPage.css'
 import email_img from '../assets/email.svg'
 import password_img from '../assets/password.svg'
+import GV_BonW_img from '../assets/GV_BonW.jpg'
 
 
 export default function LoginPage() {
 
     return (
-        <div className="container">
-            <div className="header">
-                <div className="text">Login</div>
-                <div className="underline"></div>
+        <div>
+            <div className="logo-header">
+                <img src={GV_BonW_img} alt="Company Logo" className="logo" />
+                <h1 className="company-name">Grand Voyage</h1>
             </div>
-            <div className="inputs">
-                <div className="input">
-                    <img src={email_img} alt="email svg"/>
-                    <input type="email" placeholder="Email"/>
+
+            <div className="container">
+                <div className="header">
+                    <div className="text">Login</div>
+                    <div className="underline"></div>
                 </div>
-                <div className="input">
-                    <img src={password_img} alt="password svg"/>
-                    <input type="password" placeholder="Password"/>
+                <div className="inputs">
+                    <div className="input">
+                        <img src={email_img} alt="email svg" />
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="input">
+                        <img src={password_img} alt="password svg" />
+                        <input type="password" placeholder="Password" />
+                    </div>
                 </div>
+                <div className="submitContainer">
+                    <div className="submit">
+                        Login
+                    </div>
+                </div>
+                <div className="sign-up">Don't have an account? <span>Sign-up here!</span></div>
             </div>
-            <div className="submitContainer">
-                <div className="submit">
-                    Login
-                </div>
-            </div>
-            <div className="sign-up">Don't have an account? <span>Sign-up here!</span></div>
         </div>
     )
 }
