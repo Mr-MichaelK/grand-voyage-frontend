9f1108115715fe8e82a291dcfabdce32b4c53bc6
Malek Baghdadi <malekbaghdade@gmail.com>
2025-04-06 19:21:40 +0300

    Styles the navbar

--- src/UserPortal/Components/Navbar.jsx
+++ src/UserPortal/Components/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import style from './Navbar.module.css'
 import travelCase from '../../assets/travelCase.svg'
 import userProfile from '../../assets/person.svg'
-import GV_BonW_img from '../../assets/GV_BonW.jpg'
+import GV_WonT_L_img from '../../assets/GV_WonT_L.png'
 
 
 export default function Navbar() {
 
     return (
-        <div>
+        <div className={style.navbar}>
             <div className={style.logoContainer}>
-                <img src={GV_BonW_img} alt="Company Logo" className={style.logo} />
+                <img src={GV_WonT_L_img} alt="Company Logo" className={style.logo} />
                 <h1 className={style.companyName}>Grand Voyage</h1>
             </div>
             <div className={style.buttonContainer}>
@@ -22,8 +22,14 @@ export default function Navbar() {
                 <button>Contact Us</button>
             </div>
             <div className={style.profileContainer}>
-                <img src={travelCase} alt='Travel case SVG' />
-                <img src={userProfile} alt='User profile SVG' />
+                <div className={style.profileIcon}>
+                    <img className={style.travelCase} src={travelCase} alt='Travel case SVG' />
+                    <span className={style.tooltip} data-tooltip="My Bookings">My Bookings</span>
+                </div>
+                <div className={style.profileIcon}>
+                    <img className={style.userProfile} src={userProfile} alt='User profile SVG' />
+                    <span className={style.tooltip} data-tooltip="My Account">My Account</span>
+                </div>
             </div>
         </div>
     )
