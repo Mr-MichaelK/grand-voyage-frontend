45d6a6bdcf464868cbcd46106c7ea8389fae4320
MrKrow <michael1kolanjian2@gmail.com>
2025-04-11 20:48:49 +0300

    Fix the commit issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,6 @@ import LoginPage from './LoginPage/LoginPage'
 import SignUpPage from './SignUpPage/SignUpPage'
 import UserPortal from './UserPortal/UserPortal'
 import './App.css'
-import Navbar from './UserPortal/Components/Navbar'
-import HotelSidebar from './UserPortal/Components/Sidebar/HotelSidebar'
-import FlightSidebar from './UserPortal/Components/Sidebar/FlightSidebar'
-import CruiseSidebar from './UserPortal/Components/Sidebar/CruiseSidebar'
-import PackageSidebar from './UserPortal/Components/Sidebar/PackageSidebar'
-import HotelCard from './UserPortal/Components/Cards/HotelCard'
-import FlightCard from './UserPortal/Components/Cards/FlightCard'
-import CruiseCard from './UserPortal/Components/Cards/CruiseCard'
-import PackageCard from './UserPortal/Components/Cards/PackageCard'
 
 function App() {
   const [currentPage, setCurrentPage] = useState('login');
@@ -30,8 +21,5 @@ function App() {
 
   return <>{page}</>;
 }
-<UserPortal>
-  <PackageCard />
-</UserPortal>
 
 export default App
