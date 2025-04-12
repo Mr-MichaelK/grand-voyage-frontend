857a6ae15f05224be2ba7eba155936aec44decb1
MrKrow <michael1kolanjian2@gmail.com>
2025-04-12 09:44:29 +0300

    Create temporary package cards to test styling

--- src/UserPortal/Components/Activities/PackagesActivity/PackagesActivity.jsx
+++ src/UserPortal/Components/Activities/PackagesActivity/PackagesActivity.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PackageSidebar from '../../Sidebar/PackageSidebar'
+import PackagesBody from '../../ActivityBodies/PackagesBody'
 
 export default function PackagesActivity() {
     return (
         <>
             <PackageSidebar></PackageSidebar>
+            <PackagesBody></PackagesBody>
         </>
     )
 }
