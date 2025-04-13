d9397992c421d8c926ba030f604e9015281dbb47
MrKrow <michael1kolanjian2@gmail.com>
2025-04-13 19:34:36 +0300

    Remove unnecessary import clutter from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,6 @@ import LoginPage from './LoginPage/LoginPage'
 import SignUpPage from './SignUpPage/SignUpPage'
 import UserPortal from './UserPortal/UserPortal'
 import './App.css'
-import Navbar from './UserPortal/Components/Navbar'
-import HotelSidebar from './UserPortal/Components/Sidebar/HotelSidebar'
-import FlightSidebar from './UserPortal/Components/Sidebar/FlightSidebar'
-import CruiseSidebar from './UserPortal/Components/Sidebar/CruiseSidebar'
-import PackageSidebar from './UserPortal/Components/Sidebar/PackageSidebar'
-import HotelCard from './UserPortal/Components/Cards/HotelCard'
-import FlightCard from './UserPortal/Components/Cards/FlightCard'
-import CruiseCard from './UserPortal/Components/Cards/CruiseCard'
-import PackageCard from './UserPortal/Components/Cards/PackageCard'
-import AboutUsActivity from './UserPortal/Components/Activities/AboutUsActivity/AboutUsActivity'
 
 function App() {
   const [currentPage, setCurrentPage] = useState('login');
@@ -31,14 +21,5 @@ function App() {
 
   return <>{page}</>;
 }
-/*
-function App() {
-  return (
-    <div className="App">
-      <AboutUsActivity />
-    </div>
-  );
-}
-  */
 
 export default App
